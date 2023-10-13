411d0bbfcaba051b612b2cb4cc9842c1c28f73ff
Darren You <jy37559624@gmail.com>
2023-10-13 23:45:30 +0800

    完成借還書功能

--- frontend/src/pages/borrowing-record.vue
+++ frontend/src/pages/borrowing-record.vue
@@ -1,42 +1,68 @@
 <script setup>
+import Cookies from 'js-cookie';
+import jwt_decode from 'jwt-decode';
 
-const desserts = [
-  {
-    dessert: 'Frozen Yogurt',
-    calories: 159,
-    fat: 6,
-    carbs: 24,
-    protein: 4,
-  },
-  {
-    dessert: 'Ice cream sandwich',
-    calories: 237,
-    fat: 6,
-    carbs: 24,
-    protein: 4,
-  },
-  {
-    dessert: 'Eclair',
-    calories: 262,
-    fat: 6,
-    carbs: 24,
-    protein: 4,
-  },
-  {
-    dessert: 'Cupcake',
-    calories: 305,
-    fat: 6,
-    carbs: 24,
-    protein: 4,
-  },
-  {
-    dessert: 'Gingerbread',
-    calories: 356,
-    fat: 6,
-    carbs: 24,
-    protein: 4,
-  },
-]
+const books = ref([]);
+const getBorrowingRecord = () => {
+  const tokenWithoutBearer = Cookies.get('jwt_token').replace('Bearer ', '');
+    // 解析JWT Token
+    const decodedToken = jwt_decode(tokenWithoutBearer);
+
+    // 获取subject
+    const userId = decodedToken.sub;
+    console.log('userId:', userId);
+
+  fetch("http://localhost:8080/borrow_records_user?userId=" + userId)
+    .then(response => response.json())
+    .then(data => {
+      books.value = data;
+    })
+    .catch(error => console.log('error', error));
+};
+
+const returnBook = (id, inventoryId) => {
+  // const tokenWithoutBearer = Cookies.get('jwt_token').replace('Bearer ', '');
+  //   // 解析JWT Token
+  //   const decodedToken = jwt_decode(tokenWithoutBearer);
+
+  //   // 获取subject
+  //   const userId = decodedToken.sub;
+  //   console.log('userId:', userId);
+
+  console.log(id)
+
+  const data = new FormData();
+  data.append('id', id);
+
+  fetch("http://localhost:8080/return_borrow_record", {
+    method: "PUT",
+    body: data
+  })
+    .then(response => {
+      updateBookStatus(inventoryId)
+    })
+    .catch(error => console.log('error', error));
+};
+
+const updateBookStatus = (inventoryId) => {
+  const data = new FormData();
+  data.append('id', inventoryId);
+  data.append('status', "IN_STOCK"); // 添加 bookId 到 FormData
+
+  fetch("http://localhost:8080/update_inventory", {
+    method: "PUT",
+    body: data
+  })
+    .then(response => {
+      window.location.reload();
+    })
+    .catch(error => console.log('error', error));
+};
+
+onMounted(() => {
+  console.log("onMounted books");
+  getBorrowingRecord();
+});
 </script>
 
 <template>
@@ -49,40 +75,37 @@ const desserts = [
         <VTable height="100%">
           <thead>
             <tr>
-              <th class="text-uppercase">
-                Desserts(100g Servings)
+              <th class="text-uppercase text-center">
+                bookId 
               </th>
               <th class="text-uppercase text-center">
-                calories
+                Borrow Date
               </th>
               <th class="text-uppercase text-center">
-                Fat(g)
+                Return Date
               </th>
               <th class="text-uppercase text-center">
-                Carbs(g)
-              </th>
-              <th class="text-uppercase text-center">
-                protein(g)
+                Action
               </th>
             </tr>
           </thead>
 
           <tbody>
-            <tr v-for="item in desserts" :key="item.dessert">
+            <tr v-for="item in books" :key="item.id">
               <td>
-                {{ item.dessert }}
+                {{ item.bookId }}
               </td>
               <td class="text-center">
-                {{ item.calories }}
+                {{ item.borrowDate }}
               </td>
               <td class="text-center">
-                {{ item.fat }}
+                {{ item.returnDate }}
               </td>
-              <td class="text-center">
-                {{ item.carbs }}
-              </td>
-              <td class="text-center">
-                {{ item.protein }}
+              <td>
+                <VBtn v-if="item.returnDate === null" 
+                  @click="returnBook(item.id, item.inventoryId)">
+                  Return
+                </VBtn>
               </td>
             </tr>
           </tbody>
