6798de2ea15aba640b355b0ca24744176a7e6a7c
Darren You <jy37559624@gmail.com>
2023-10-14 00:17:31 +0800

    initial data

--- frontend/src/pages/dashboard.vue
+++ frontend/src/pages/dashboard.vue
@@ -20,7 +20,80 @@ const newProject = {
   subtitle: 'Yearly Project',
 }
 
+const initialData = (() => {
+  insertUsers("0911111111", "abc");
+  insertUsers("0922222222", "abc");
+  insertUsers("0933333333", "abc");
+
+  //  books
+  insertBooks(1, "賈伯斯傳（紀念增訂版", "Walter Isaacson", "人人都知道賈伯斯不遺餘力捍衛隱私，這位傳奇企業家從未寫過回憶錄，但自2009 年起，他接受本書作者艾薩克森多達四十餘次的深入訪談，並允許艾薩克森遍訪他的朋友、親戚、競爭對手、仇人和同事，總數超過一百人。");
+  insertBooks(2, "馬斯克傳", "華特．艾薩克森", "除了透過推特、新聞、甚至網路迷因，還有其他方式可以看懂馬斯克嗎？曾操刀《賈伯斯傳》的艾薩克森獲得馬斯克首肯，進入他的童年與精神世界，暴露他無止盡的缺點、以及心靈黑暗的來源。");
+  insertBooks(3, "多巴胺國度：在縱慾年代找到身心平衡", "安娜‧蘭布克醫師（Dr. Anna Lembke） ", "多巴胺是一種神經傳導物質，又稱為「腦內啡」，要讓大腦釋放多巴胺很簡單，只要你做一件事情能讓自己開心就行了，像是上網、吃喝玩樂、盡情購物。不過，多巴胺會逐漸減少，需要更多的慾望、更強的刺激才行，也因此讓人愈來愈痛苦。");
+
+  //  inventory
+  insertInventory(1, 1)
+  insertInventory(2, 2)
+  insertInventory(3, 3)
+
+})
+
+const insertUsers = ((phone, password) => {
+  var formdata = new FormData();
+  formdata.append("phone", phone);
+  formdata.append("password", password);
+
+  var requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+  };
+
+  fetch("http://localhost:8080/register", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+})
+
+const insertBooks = ((isbn, name, author, introduction) => {
+  var formdata = new FormData();
+  formdata.append("isbn", isbn);
+  formdata.append("name", name);
+  formdata.append("author", author);
+  formdata.append("introduction", introduction);
+
+  var requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+  };
+
+  fetch("http://localhost:8080/insert_book", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+})
+
+const insertInventory = ((isbn, bookId) => {
+  var formdata = new FormData();
+formdata.append("isbn", isbn);
+formdata.append("bookId", bookId);
+
+var requestOptions = {
+  method: 'POST',
+  body: formdata,
+  redirect: 'follow'
+};
+
+fetch("http://localhost:8080/insert_inventory", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+})
+
 onMounted(() => {
+  initialData();
+
+
   console.log(Cookies.get('jwt_token'));
   // 移除 "Bearer " 前缀
   const tokenWithoutBearer = Cookies.get('jwt_token').replace('Bearer ', '');
@@ -117,6 +190,11 @@ onMounted(() => {
     </VCol> -->
     <VCol cols="12">
       <h1>Library MGMT landing page</h1>
+    </VCol>
+    <VCol cols="12">
+      <VBtn @click="initialData">
+        Insert demo data
+      </VBtn>
     </VCol>
   </VRow>
 </template>
